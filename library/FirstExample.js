41c1d879f0ca667ea2f05d6ce4083cbd592a066e
Shizqu <60578896+Shizqu@users.noreply.github.com>
2022-02-04 09:33:22 +0100

    jqury

--- library/FirstExample.js
+++
@@ -1,9 +0,0 @@
-$(function (){
-    $('#page').css('background-color', 'ivory');
-    $('h2').css('letter-spacing', '5px');
-    $('.link').css({
-        'color': 'black',
-        'text-ddecoration': 'none',
-    });
-    $('[href="#"]').attr('href','https://pl.wikipedia.org/wiki/J%C4%99zyk_programowania');
-});
