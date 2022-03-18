e99e72f56d95fd18f79eb7fd6cbe95cc9acf3c10
Shizqu <60578896+Shizqu@users.noreply.github.com>
2022-03-18 12:07:17 +0100

    jqury

--- js/events.js
+++ js/events.js
@@ -1,5 +1,26 @@
-$(function (){
-    $('input').on('focus', function (){$('span').eq(0).css('display', 'inline').fadeOut(4500);});
-    $('input').on('blur', function (){$('span').eq(1).css('display', 'inline').fadeOut(4500);});
-    $('input').on('change', function (){$('span').eq(2).css('display', 'inline').fadeOut(4500);});
+$(function(){
+    let i=0;
+    i++;   let n=0;
+    $('#wew').mouseover(function(){
+        i += 1;
+        $('#komunikat').text('najechano na porostokąt ' + i + ' razy');
+    }).mouseout(function(){
+        $('#komunikat').text('opuszczono obszar prostokąta ');
+    });
+    $('#wew').click(function(){
+        n+=1;
+        $('#komunikat').text('Kliknieto prostokat ' + n + 'razy');
+    });
+});
+
+$(function(){
+    let i=0;
+    let n=0;
+    $('#tekst').on('scroll', function(){
+        $('span').eq(5).text(i+=1);
+    });
+
+    $(window).on('resize', function(){
+        $('span').eq(6).text(n+=1);
+    });
 });
--- js/secondcounter.js
+++
@@ -1,9 +0,0 @@
-var timeleft = 10;
-var downloadTimer = setInterval(function(){
-    if(timeleft <= 0){
-        clearInterval(downloadTimer);
-    }
-    document.getElementById("progressBar").value = 10 - timeleft;
-    timeleft -= 1;
-}, 1000);
-<progress value="0" max="10" id="progressBar"></progress>
